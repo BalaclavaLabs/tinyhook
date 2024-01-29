ad2eb43f5c23e0e1e5e1ad9794b5ed97d48e5fb9
Sabotage <sabotageresearch@proton.me>
2024-01-29 10:41:07 -0600

    rewrite logging, more refactoring

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 type Config struct {
@@ -30,6 +32,22 @@ type Config struct {
 	Directory    string `json:"directory"`
 	NginxVersion string `json:"nginx_version"`
 	Processes    map[string]*os.Process
+	Loggers      map[string]*os.File
+}
+
+func (c Config) Logger(app string, command string) *os.File {
+	f := strings.Join(strings.Split(command, " "), "-")
+	n := fmt.Sprintf("%s.%s.%s.log", app, f, time.Now().Format(time.UnixDate))
+	p := fmt.Sprintf("%s/.log/%s", c.Directory, n)
+
+	l, err := os.OpenFile(p, os.O_CREATE, fs.ModePerm)
+
+	if err != nil {
+		log.Printf("Couldn't Open Log %s Piping to STDERR", p)
+		return os.Stderr
+	}
+
+	return l
 }
 
 func (c Config) RepoUrl(name string) *url.URL {
@@ -40,6 +58,10 @@ func (c Config) RepoUrl(name string) *url.URL {
 		log.Fatal("Invalid Repo URL")
 	}
 	return u
+}
+
+func (c Config) AppDir(name string) string {
+	return fmt.Sprintf("%s/%s", c.Directory, c.RepoUrl(name).Path)
 }
 
 func (c *Config) Init() {
@@ -56,108 +78,128 @@ func (c *Config) Init() {
 		os.Remove(c.Directory)
 		os.Mkdir(c.Directory, os.ModePerm)
 	}
-	if (c.Processes == nil) {
+	log_dir := fmt.Sprintf("%s/.log", c.Directory)
+	info, err = os.Stat(log_dir)
+	if err != nil {
+		os.Mkdir(log_dir, os.ModePerm)
+		info, _ = os.Stat(c.Directory)
+	}
+	if !info.IsDir() {
+		os.Remove(log_dir)
+		os.Mkdir(log_dir, os.ModePerm)
+	}
+	if c.Processes == nil {
 		c.Processes = map[string]*os.Process{}
 	}
-	for name, _ := range c.Apps {
+	if c.Loggers == nil {
+		c.Loggers = map[string]*os.File{}
+	}
+	for name := range c.Apps {
 		c.StartProcess(name)
 	}
 }
 
-func (c Config) PushProcess (name string, proc *os.Process) {
+func (c Config) PushProcess(name string, proc *os.Process) {
 	c.Processes[name] = proc
 }
 
-func (c Config) StartProcess (name string) {
+func (c Config) PushLogger(name string, out *os.File) {
+	c.Loggers[name] = out
+}
+
+func (c Config) StartProcess(name string) {
+	c.Clone(name)
+	c.Checkout(name)
+	c.Pull(name)
+	c.RunBuild(name)
+	c.RunEntry(name)
+}
+
+func (c Config) Clone(name string) {
+	_, err := os.Stat(c.AppDir(name) + "/.git")
+	if err != nil {
+		log.Printf("No GIT Repo Detected for %s", name)
+		app := c.Apps[name]
+		dir := c.AppDir(name)
+		cmd := exec.Command("git", "clone", app.Repo, dir)
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		log.Printf("Cloning %s from %s into %s", name, app.Repo, dir)
+		cmd.Run()
+	}
+}
+
+func (c Config) Checkout(name string) {
+	out := c.Logger(name, "git checkout")
 	app := c.Apps[name]
-	url := c.RepoUrl(name)
-
-	loc := fmt.Sprintf("%s/%s", c.Directory, url.Path)
-
-	cmd := exec.Command("git", "clone", app.Repo, loc)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	log.Printf("now running git clone %s %s", app.Repo, loc)
-	cmd.Run()
-
-	cmd = exec.Command("git", "checkout", app.Branch)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	cmd.Dir = loc
+	cmd := exec.Command("git", "checkout", app.Branch)
+	cmd.Stderr = out
+	cmd.Stdout = out
+	cmd.Dir = c.AppDir(name)
 	log.Printf("now running git checkout %s", app.Branch)
 	cmd.Run()
+	out.Close()
+}
 
-	cmd = exec.Command("git", "pull")
-	cmd.Dir = loc
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	log.Print("now running git pull")
+func (c Config) Pull(name string) {
+	out := c.Logger("name", "git pull")
+	app := c.Apps[name]
+	cmd := exec.Command("git", "pull")
+	cmd.Dir = c.AppDir(name)
+	cmd.Stdout = out
+	cmd.Stderr = out
+	log.Printf("Pulling latest from %s", app.Repo)
 	cmd.Run()
+	out.Close()
+}
 
-	cmd = exec.Command(app.Build[0], app.Build[1:]...)
-	cmd.Dir = loc
+func (c Config) RunBuild(name string) {
+	out := c.Logger(name, "build")
+	app := c.Apps[name]
+	cmd := exec.Command(app.Build[0], app.Build[1:]...)
+	cmd.Dir = c.AppDir(name)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	log.Printf("now running %s", strings.Join(app.Build, " "))
 	cmd.Run()
+	out.Close()
+}
 
-	cmd = exec.Command(app.Entry[0], app.Entry[1:]...)
-	cmd.Env = c.BuildEnv(name)
-	cmd.Dir = loc
+func (c Config) RunEntry(name string) {
+	app := c.Apps[name]
+	cmd := exec.Command(app.Entry[0], app.Entry[1:]...)
+	cmd.Dir = c.AppDir(name)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
-	log.Printf("now running %s", strings.Join(app.Entry, " "))
+	cmd.Env = c.BuildEnv(name)
+	log.Printf("Now starting %s from entry %s", name, strings.Join(app.Entry, " "))
 	cmd.Start()
 
 	c.PushProcess(name, cmd.Process)
 }
 
-func (c Config) RestartProcess(name string) {
-	log.Print("restarting process")
+func (c Config) Kill(name string) {
 	proc := c.Processes[name]
 	if proc != nil {
-		log.Print("Killing Process ", proc.Pid)
+		log.Printf("Killing Process %d", proc.Pid)
 		cmd := exec.Command("kill", fmt.Sprintf("%d", proc.Pid))
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		cmd.Run()
 	}
+	out := c.Loggers[name]
+	if out != nil {
+		out.Close()
+	}
+}
 
-	url := c.RepoUrl(name)
-	app := c.Apps[name]
-
-	loc := fmt.Sprintf("%s/%s", c.Directory, url.Path)
-
-	cmd := exec.Command("git", "checkout", app.Branch)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	cmd.Dir = loc
-	log.Printf("now running git checkout %s", app.Branch)
-	cmd.Run()
-
-	cmd = exec.Command("git", "pull")
-	cmd.Dir = loc
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	log.Print("now running git pull")
-	cmd.Run()
-
-	cmd = exec.Command(app.Build[0], app.Build[1:]...)
-	cmd.Dir = loc
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	log.Printf("now running ", strings.Join(app.Build, " "))
-	cmd.Run()
-
-	cmd = exec.Command(app.Entry[0], app.Entry[1:]...)
-	cmd.Dir = loc
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	cmd.Env = c.BuildEnv(name)
-	log.Printf("now running %s", strings.Join(app.Entry, " "))
-	cmd.Start()
-
-	c.PushProcess(name, proc)
+func (c Config) RestartProcess(name string) {
+	log.Printf("Restarting %s", name)
+	c.Kill(name)
+	c.Checkout(name)
+	c.Pull(name)
+	c.RunBuild(name)
+	c.RunEntry(name)
 }
 
 func (c Config) BuildEnv(name string) []string {
@@ -188,7 +230,6 @@ func (c Config) Ref(name string) string {
 	return fmt.Sprintf("refs/heads/%s", app.Branch)
 }
 
-
 func ReadConfig() Config {
 	c := Config{}
 	j, err := os.ReadFile("config.json")
@@ -206,7 +247,6 @@ func ReadConfig() Config {
 type HookHandler struct {
 	config Config
 }
-
 
 func (h HookHandler) ReadBody(r *http.Request) ([]byte, error) {
 	body, err := io.ReadAll(r.Body)
@@ -234,6 +274,7 @@ func (h HookHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		name := c.GetAppByRepo(push.Repository.CloneURL)
 
 		if push.Ref == c.Ref(name) {
+			log.Printf("PUSH Detected on %s @ %s", push.Repository.FullName, push.Ref)
 			c.RestartProcess(name)
 		}
 	}
